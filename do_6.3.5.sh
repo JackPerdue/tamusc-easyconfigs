ec83a6519047edff813e0499f01480800a9c94a3
Jack Perdue <j-perdue@tamu.edu>
2014-10-10 08:01:11 -0500

    new (re)builds on ada

---
+++ do_6.3.5.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for x in `find . -type d | sort -r` ; do
+  latest=`ls $x/*-ictce-6.?.5*.eb 2>/dev/null | sort -n | tail -1`
+  if [ "$latest" != "" ] ; then
+    echo $latest | grep -e Python -e test >& /dev/null
+    if [ $? -ne 0 ] ; then
+      eb --try-toolchain=ictce,6.3.5 $latest
+    fi
+  fi
+done
+# EOF
