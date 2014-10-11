61acf980e80aec2d6f9dff33821fc5e1f2479ecb
Jack Perdue <j-perdue@tamu.edu>
2014-10-11 08:44:08 -0500

    updates after cleaning hpcugent tree

---
+++ cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+TMP=/tmp/cu$$.tmp
+JKPECDIR=/software/easybuild/tamusc/githubs/jack/easybuild-easyconfigs/easybuild/easyconfigs # what to clean
+EBECDIR=~/github/easybuild-easyconfigs.develop/easybuild/easyconfigs/ # clean copy
+TAMUECDIR=/software/easybuild/tamusc/githubs/jack/tamusc-easyconfigs # move stuff to here
+
+cd $JKPECDIR
+for x in `diff -q -r . $EBECDIR | grep Only | grep -v '\.git' | cut -f 3,4 -d ' ' | sed 's,^\./,,' | sed 's,: ,/,'` ; do
+  if [ -f $TAMUECDIR/$x ] ; then
+    diff -q $x $TAMUECDIR/$x
+    if [ $? -eq 0 ] ; then
+      echo "$x is the same as $TAMUECDIR/$x"
+      git rm $x
+    else
+      "### DIFF: diff $x $TAMUECDIR/$x"
+    fi
+  else
+    cp $x $TAMUECDIR/$x
+    #git rm $x
+  fi
+done
+# EOF
--- do_7.1.2.sh
+++ do_7.1.2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for x in `find . -type d | sort` ; do
+for x in `find . -type d | sort -r` ; do
   latest=`ls $x/*-ictce-6.?.5*.eb 2>/dev/null | sort -n | tail -1`
   if [ "$latest" != "" ] ; then
     echo $latest | grep -e Python -e test >& /dev/null
